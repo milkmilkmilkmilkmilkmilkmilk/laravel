44618d3f1835b70951382385a7f2e4314b832e31
Artem Tyan <ahoneyfroggy@gmail.com>
2026-03-13 17:04:22 +0300

    front

--- resources/views/main/article.blade.php
+++ resources/views/main/article.blade.php
@@ -1,30 +1,46 @@
 @extends('layout') 
 @section('content')
 
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">Date</th>
-      <th scope="col">Name</th>
-      <th scope="col">ShortDesc</th>
-      <th scope="col">Desc</th>
-      <th scope="col">Preview image</th>
-    </tr>
-  </thead>
-  <tbody>
-    @foreach($articles as $article)
-    <tr>
-      <th scope="row">{{$article->date}}</th>
-      <td>{{$article->name}}</td>
-      <td>{{$article->shortDesc}}</td>
-      <td>{{$article->desc}}</td>
-      <td><a href="/full_image/{{$article->full_image}}"> 
-            <img src="{{URL::asset($article->preview_image)}}" alt=""> 
-          </a> 
-      </td>
-    </tr>
-    @endforeach
-  </tbody>
-</table>
+<div class="article-list">
+  @foreach($articles as $article)
+    <article class="article-card card mb-4 border-0 shadow-sm">
+      <div class="row g-0">
+        <div class="col-md-8">
+          <div class="card-body d-flex flex-column h-100">
+            <div class="article-meta mb-2">
+              <span class="article-date">
+                {{ $article->date }}
+              </span>
+            </div>
+
+            <h2 class="article-title h5 mb-2">
+              {{ $article->name }}
+            </h2>
+
+            <p class="article-short text-muted mb-2">
+              {{ $article->shortDesc }}
+            </p>
+
+            <p class="article-desc mb-0">
+              {{ $article->desc }}
+            </p>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="article-image-wrapper h-100 d-flex align-items-center justify-content-center p-2 p-md-3">
+            <a href="/full_image/{{ $article->full_image }}" class="article-image-link">
+              <img
+                src="{{ URL::asset($article->preview_image) }}"
+                alt="{{ $article->name }}"
+                class="img-fluid rounded article-image"
+              >
+            </a>
+          </div>
+        </div>
+      </div>
+    </article>
+  @endforeach
+</div>
 
 @endsection
